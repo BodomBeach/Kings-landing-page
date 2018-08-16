5ad823cb8e9d6713a54d93589006e4b65bb34780
Hugo Payet <hugopayetsync@gmail.com>
2018-08-17 00:14:03 +0200

    modifs routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'creatives#index'
   get 'creatives#index', to: 'users#new', as: 'form'
   get '/thankyou', to: 'creatives#thankyou'
-  post 'creatives#index', to: 'users#create'
-  get 'creatives#index2', to: 'users#new', as: 'form'
-  get 'creatives#index3', to: 'users#new', as: 'form'
+  post 'creatives/index', to: 'users#create'
+  get 'creatives/index2', to: 'users#new', as: 'form'
+  get 'creatives/index3', to: 'users#new', as: 'form'
 
 end
