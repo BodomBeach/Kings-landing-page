33899ad0d663b3c8d66084cbca499bc3eb5de063
Hugo Payet <hugopayetsync@gmail.com>
2018-08-17 01:10:41 +0200

    modifs routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
   root 'creatives#index'
   get '/thankyou', to: 'creatives#thankyou'
   get '/index2', to: 'creatives#index2'
-  get '/index3', to: 'creatives#index2'
+  get '/index3', to: 'creatives#index3'
   post '/index', to: 'users#create', as: 'form'
 end
 
