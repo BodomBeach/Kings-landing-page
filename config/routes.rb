213eb9aeefe1c43b054e319ae75626c008db068f
Hugo Payet <hugopayetsync@gmail.com>
2018-08-17 01:04:24 +0200

    je tente des trucs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
   root 'creatives#index'
   get '/thankyou', to: 'creatives#thankyou'
   get '/index1', to: 'users#new'
-  get '/index2', to: 'users#new'
+  get '/index2', to: 'cratives#index2'
   get '/index3', to: 'users#new'
   post '/index', to: 'users#create', as: 'form'
 end
