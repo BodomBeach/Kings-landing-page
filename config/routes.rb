8f73f5b2c3ca19b6ead20be729c12ece603fb0bb
Hugo Payet <hugopayetsync@gmail.com>
2018-08-17 00:27:51 +0200

    fatigue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'creatives#index'
-  get 'creatives#index', to: 'users#new'
+  get 'creatives#index', to: 'users#new', as: 'form'
   get '/thankyou', to: 'creatives#thankyou'
   post 'creatives/index', to: 'users#create'
   get 'creatives/index2', to: 'users#new'
